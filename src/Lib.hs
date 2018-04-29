dd145cbabafafb2fa824bf7efd331a1c3c846481
ocramz <zocca.marco@gmail.com>
2018-04-29 17:11:04 +0200

    messing with TH

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,20 +1,44 @@
 {-# language TemplateHaskell, DataKinds, FlexibleContexts #-}
 module Lib where
 
+import Control.Monad.IO.Class
+import Control.Monad.Catch
+
 import qualified Control.Foldl as L
-import Data.Vinyl (rcast)
-import Frames
-import Frames.CSV
+import Data.Vinyl (Rec(..), rcast)
+import Frames (Frame(..), Record(..), tableTypes, tableTypes', inCoreAoS, readTable, runcurry')
+import Frames.CSV -- (RowGen(..), rowGen, columnNames, tablePrefix, rowTypeName)
 import Data.Text
+import Pipes.Core
+
+import Language.Haskell.TH -- (runQ)
+import Language.Haskell.TH.Syntax
 
 
 q1path :: String
 q1path = "april-5-2018/data/q1"
 
-tableTypes' (rowGen "april-5-2018/data/q1"){
-    rowTypeName = "Q1"
-  , columnNames = [ "Label", "Count" ]
-  , tablePrefix = "NoHead"}
 
-loadRows :: IO (Frame Q1)
-loadRows = inCoreAoS (readTable q1path)
+-- tableTypes "Row" "april-5-2018/data/q1"
+
+tableTypes' (rowGen "april-5-2018/data/q1"){
+    rowTypeName = "Q1Row"
+  , columnNames = [ "Label", "Count" ]
+  , tablePrefix = "Q1"}
+
+-- baz :: Q [Dec]
+-- baz = runQ (
+  -- tableTypes' (rowGen "april-5-2018/data/q1"){
+  --   rowTypeName = "Q1Row"
+  -- , columnNames = [ "Label", "Count" ]
+  -- , tablePrefix = "Q1"}
+  -- )
+
+-- loadRows :: IO (Frame Q1Row)
+-- loadRows = inCoreAoS (readTable q1path)
+
+
+-- rowCount :: Record '[Q1Label, Q1Count] -> Double
+-- rowCount = runcurry' (\n m -> fromIntegral m)
+
+-- -- totalQ1 = rowLabel <$> loadRows 
